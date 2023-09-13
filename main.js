4c94dc11412ef5d6957908bc04857e7c1cfb56f6
Your Name <you@examaple.com>
2023-09-13 17:18:09 -0500

    initial

---
+++ main.js
@@ -0,0 +1,312 @@
+var areasPiso1 = {
+    '55': 'Gimnasio',
+    '56': 'Gimnasio',
+    '75': 'Gimnasio',
+    '76': 'Gimnasio',
+    '95': 'Gimnasio',
+    '96': 'Gimnasio',
+    '115': 'Gimnasio',
+    '116': 'Gimnasio',
+    '135': 'Gimnasio',
+    '136': 'Gimnasio',
+    '155': 'Gimnasio',
+    '156': 'Gimnasio',
+    '175': 'Gimnasio',
+    '176': 'Gimnasio',
+
+    '92' : 'Auditorio',
+    '93' : 'Auditorio',
+    '112' : 'Auditorio',
+    '113' : 'Auditorio',
+    '132' : 'Auditorio',
+    '133' : 'Auditorio',
+    '152' : 'Auditorio',
+    '153' : 'Auditorio',
+    '172' : 'Auditorio',
+    '173' : 'Auditorio',
+    '192' : 'Auditorio',
+    '193' : 'Auditorio',
+
+    '44' : 'Administración Educativa',
+    '45' : 'Administración Educativa',
+    '64' : 'Administración Educativa',
+    '65' : 'Administración Educativa',
+
+    '48' : 'Fondo Emprender',
+    '49' : 'Fondo Emprender',
+    '67' : 'Fondo Emprender',
+    '68' : 'Fondo Emprender',
+    '69' : 'Fondo Emprender',
+    '88' : 'Fondo Emprender',
+    '89' : 'Fondo Emprender',
+
+    '83' : 'Senaverso',
+    '103' : 'Senaverso',
+    '123' : 'Senaverso',
+    '143' : 'Senaverso',
+    '163' : 'Senaverso',
+
+}
+
+var areasPiso2 = {
+    '41' : 'Biblioteca',
+    '42' : 'Biblioteca',
+    '43' : 'Biblioteca',
+    '61' : 'Biblioteca',
+    '62' : 'Biblioteca',
+    '63' : 'Biblioteca',
+    '81' : 'Biblioteca',
+    '82' : 'Biblioteca',
+    '83' : 'Biblioteca',
+    '101' : 'Biblioteca',
+    '102' : 'Biblioteca',
+    '103' : 'Biblioteca',
+
+    '105' : 'Agencia Pública de Empleo',
+    '106' : 'Agencia Pública de Empleo',
+    '125' : 'Agencia Pública de Empleo',
+    '126' : 'Agencia Pública de Empleo',
+
+    '135' : 'Sennova',
+    '136' : 'Sennova',
+    '155' : 'Sennova',
+    '156' : 'Sennova',
+
+}
+
+var areasPiso3 = {
+    '6' : 'Teleinformática',
+    '7' : 'Teleinformática',
+    '8' : 'Teleinformática',
+    '26' : 'Teleinformática',
+    '27' : 'Teleinformática',
+    '28' : 'Teleinformática',
+
+}
+
+var areasPiso4 = {
+    '5' : 'Sennova',
+    '6' : 'Sennova',
+    '7' : 'Sennova',
+    '25' : 'Sennova',
+    '26' : 'Sennova',
+    '27' : 'Sennova',
+    '45' : 'Sennova',
+    '46' : 'Sennova',
+    '47' : 'Sennova',
+
+    '85' : 'Control Numérico Computarizado',
+    '86' : 'Control Numérico Computarizado',
+    '87' : 'Control Numérico Computarizado',
+    '105' : 'Control Numérico Computarizado',
+    '106' : 'Control Numérico Computarizado',
+    '107' : 'Control Numérico Computarizado',
+    '125' : 'Control Numérico Computarizado',
+    '126' : 'Control Numérico Computarizado',
+    '127' : 'Control Numérico Computarizado',
+    '145' : 'Control Numérico Computarizado',
+    '146' : 'Control Numérico Computarizado',
+    '147' : 'Control Numérico Computarizado',
+
+    '165' : 'Alturas',
+    '166' : 'Alturas',
+    '185' : 'Alturas',
+    '186' : 'Alturas',
+
+    '168' : 'Bienestar al Aprendíz',
+    '188' : 'Bienestar al Aprendíz',
+
+    '169' : 'Enfermería',
+    '189' : 'Enfermería',
+
+    '50' : 'Mecánica Industrial',
+    '51' : 'Mecánica Industrial',
+    '52' : 'Mecánica Industrial',
+    '70' : 'Mecánica Industrial',
+    '71' : 'Mecánica Industrial',
+    '72' : 'Mecánica Industrial',
+    '90' : 'Mecánica Industrial',
+    '91' : 'Mecánica Industrial',
+    '92' : 'Mecánica Industrial',
+    '110' : 'Mecánica Industrial',
+    '111' : 'Mecánica Industrial',
+    '112' : 'Mecánica Industrial',
+    '130' : 'Mecánica Industrial',
+    '131' : 'Mecánica Industrial',
+    '132' : 'Mecánica Industrial',
+    '150' : 'Mecánica Industrial',
+    '151' : 'Mecánica Industrial',
+    '152' : 'Mecánica Industrial',
+
+    '176' : 'Festo',
+    '177' : 'Festo',
+    '196' : 'Festo',
+    '197' : 'Festo',
+
+    '170' : 'Fabricación Mecánica',
+    '171' : 'Fabricación Mecánica',
+    '172' : 'Fabricación Mecánica',
+    '190' : 'Fabricación Mecánica',
+    '191' : 'Fabricación Mecánica',
+    '192' : 'Fabricación Mecánica',
+
+    '173' : 'Simulador de Maquinaria Pesada',
+    '174' : 'Simulador de Maquinaria Pesada',
+    '175' : 'Simulador de Maquinaria Pesada',
+    '190' : 'Simulador de Maquinaria Pesada',
+    '191' : 'Simulador de Maquinaria Pesada',
+    '192' : 'Simulador de Maquinaria Pesada',
+
+    '36' : 'Automatización',
+    '37' : 'Automatización',
+    '56' : 'Automatización',
+    '57' : 'Automatización',
+    '76' : 'Automatización',
+    '77' : 'Automatización',
+    '96' : 'Automatización',
+    '97' : 'Automatización',
+    '116' : 'Automatización',
+    '117' : 'Automatización',
+    '136' : 'Automatización',
+    '137' : 'Automatización',
+    '156' : 'Automatización',
+    '157' : 'Automatización',
+
+}
+
+var areasPiso5 = {
+    '1' : 'Electricidad',
+    '2' : 'Electricidad',
+    '3' : 'Electricidad',
+    '21' : 'Electricidad',
+    '22' : 'Electricidad',
+    '23' : 'Electricidad',
+    '41' : 'Electricidad',
+    '42' : 'Electricidad',
+    '43' : 'Electricidad',
+    '61' : 'Electricidad',
+    '62' : 'Electricidad',
+    '63' : 'Electricidad',
+    '81' : 'Electricidad',
+    '82' : 'Electricidad',
+    '83' : 'Electricidad',
+    '101' : 'Electricidad',
+    '102' : 'Electricidad',
+    '103' : 'Electricidad',
+    '121' : 'Electricidad',
+    '122' : 'Electricidad',
+    '123' : 'Electricidad',
+    '141' : 'Electricidad',
+    '142' : 'Electricidad',
+    '143' : 'Electricidad',
+    '161' : 'Electricidad',
+    '162' : 'Electricidad',
+    '163' : 'Electricidad',
+    '181' : 'Electricidad',
+    '182' : 'Electricidad',
+    '183' : 'Electricidad',
+    
+    '5' : 'Multimedia',
+    '6' : 'Multimedia',
+    '7' : 'Multimedia',
+    '8' : 'Multimedia',
+    '9' : 'Multimedia',
+    '29' : 'Multimedia',
+    '30' : 'Multimedia',
+    '31' : 'Multimedia',
+    '32' : 'Multimedia',
+    '33' : 'Multimedia',
+    '34' : 'Multimedia',
+    '35' : 'Multimedia',
+    '36' : 'Multimedia',
+    '53' : 'Multimedia',
+    '54' : 'Multimedia',
+    '55' : 'Multimedia',
+    '56' : 'Multimedia',
+    '57' : 'Multimedia',
+    '58' : 'Multimedia',
+    '77' : 'Multimedia',
+    '78' : 'Multimedia',
+
+    '47' : 'Soldadura',
+    '48' : 'Soldadura',
+    '49' : 'Soldadura',
+    '50' : 'Soldadura',
+    '67' : 'Soldadura',
+    '68' : 'Soldadura',
+    '69' : 'Soldadura',
+    '70' : 'Soldadura',
+    '87' : 'Soldadura',
+    '88' : 'Soldadura',
+    '89' : 'Soldadura',
+    '90' : 'Soldadura',
+    '107' : 'Soldadura',
+    '108' : 'Soldadura',
+    '109' : 'Soldadura',
+    '110' : 'Soldadura',
+
+}
+
+var areasPiso6 = {
+    '69' : 'Diseño Arquitectónico',
+    '70' : 'Diseño Arquitectónico',
+    '71' : 'Diseño Arquitectónico',
+    '89' : 'Diseño Arquitectónico',
+    '90' : 'Diseño Arquitectónico',
+    '91' : 'Diseño Arquitectónico',
+    '109' : 'Diseño Arquitectónico',
+    '110' : 'Diseño Arquitectónico',
+    '111' : 'Diseño Arquitectónico',
+    '129' : 'Diseño Arquitectónico',
+    '130' : 'Diseño Arquitectónico',
+    '131' : 'Diseño Arquitectónico',
+    '149' : 'Diseño Arquitectónico',
+    '150' : 'Diseño Arquitectónico',
+    '151' : 'Diseño Arquitectónico',
+    '169' : 'Diseño Arquitectónico',
+    '170' : 'Diseño Arquitectónico',
+    '171' : 'Diseño Arquitectónico',
+
+}
+
+
+
+let areaId = "";
+
+let pisoId = "";
+
+function identifyLevel(piso) {
+    pisoId = piso;
+}
+
+function navigateTo(area) {
+    areaId = area;
+    //console.log(pisoId);
+    //console.log(area);
+    document.getElementById('output').innerText = "";
+
+    if (pisoId == '1' && areaId in areasPiso1) {
+        //console.log(areasPiso1[areaId]);
+        document.getElementById('output').innerText = `${areasPiso1[areaId]}`;
+    }
+    if (pisoId == '2' && areaId in areasPiso2) {
+        //console.log(areasPiso2[areaId]);
+        document.getElementById('output').innerText = `${areasPiso2[areaId]}`;
+    }
+    if (pisoId == '3' && areaId in areasPiso3) {
+        //console.log(areasPiso3[areaId]);
+        document.getElementById('output').innerText = `${areasPiso3[areaId]}`;
+    }
+    if (pisoId == '4' && areaId in areasPiso4) {
+        //console.log(areasPiso4[areaId]);
+        document.getElementById('output').innerText = `${areasPiso4[areaId]}`;
+    }
+    if (pisoId == '5' && areaId in areasPiso5) {
+        //console.log(areasPiso5[areaId]);
+        document.getElementById('output').innerText = `${areasPiso5[areaId]}`;
+    }
+    if (pisoId == '6' && areaId in areasPiso6) {
+        //console.log(areasPiso6[areaId]);
+        document.getElementById('output').innerText = `${areasPiso6[areaId]}`;
+    }
+}
